bc57ed3bcba99dc3a403dd93627852fb01d14cc8
SOFIA AINHOA <88836758+sofiayazbek@users.noreply.github.com>
2021-10-04 12:02:41 +0000

    ex3

--- 3.py
+++ 3.py
@@ -6,14 +6,19 @@
 numeros = [1, 2, 3, 4, 5]
 maior = 0
 menor = 9999999
+indice1 = 0
+indice2 = 0
 
 for i in range(5):
   if numeros[i] > maior:
     maior = numeros[i]
+    indice1 = i
   if numeros[i] < menor:
     menor = numeros[i]
+    indice2 = i
+    
 
 
 print(f"Os valores da lista são: {numeros}")
-print(f"O maior número é: {maior}")
-print(f"O menor número é: {menor}")
+print(f"O maior número é: {maior}, e está na posição: {indice1}")
+print(f"O menor número é: {menor}, e está na posição: {indice2}")
